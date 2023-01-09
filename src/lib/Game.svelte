66cb1e6ed7f8e429a9e7d7bf4c59a251ce018761
801 Txchii <64236452+801txchii@users.noreply.github.com>
2023-01-09 19:00:09 +0000

    yoyoyo

---
+++ src/lib/Game.svelte
@@ -0,0 +1,21 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+
+  const down = () => {
+    count -= 1
+  }
+</script>
+
+<h1>Clicks: {count}</h1>
+
+<button on:click={increment}>
+  +
+</button>
+
+<button on:click={down}>
+  -
+</button>
+
